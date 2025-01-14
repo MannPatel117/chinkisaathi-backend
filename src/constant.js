45f0ecf1ae1c31639d653c0064939777763fdcb5
Mann <mannp3005@gmail.com>
2025-01-14 14:45:05 +0530

    Removed constant.js

--- src/constant.js
+++
@@ -1,6 +0,0 @@
-export const DB_CONFIG = {
-    host: "localhost", // Replace with your database host
-    user: "root",      // Replace with your database user
-    password: "MM7SDnn@", // Replace with your database password
-    database: "chinki-schema", // Imported from constants.js
-};
