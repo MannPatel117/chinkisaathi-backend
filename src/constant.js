e040f331ea8b7daea2cc4e2914da492d5c5b42a4
Mann <mannp3005@gmail.com>
2025-01-14 14:41:02 +0530

    SQL Connection completed

---
+++ src/constant.js
@@ -0,0 +1,6 @@
+export const DB_CONFIG = {
+    host: "localhost", // Replace with your database host
+    user: "root",      // Replace with your database user
+    password: "MM7SDnn@", // Replace with your database password
+    database: "chinki-schema", // Imported from constants.js
+};
