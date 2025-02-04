fe54ed6d7cbd3f0327524bf735c5741f3575070b
Mann <mannp3005@gmail.com>
2025-02-05 01:56:27 +0530

    inventoryTransaction model declared

---
+++ src/model/inventoryTransaction.model.js
@@ -0,0 +1,87 @@
+import { Sequelize, DataTypes } from "sequelize";
+import { initialSequelize } from "../utils/sql.js";
+import { Inventory } from './inventory.model.js';
+import { product } from './product.model.js';
+
+const sequelize = initialSequelize();
+
+export const InventoryTransaction = (await sequelize).define('InventoryTransaction', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true, 
+    },
+    inventoryID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Inventory,
+            key: 'inventoryID'
+        },
+        onDelete: 'CASCADE'
+    },
+    productID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: product,
+            key: 'productID'
+        },
+        onDelete: 'CASCADE'
+    },
+    accountTransactionID: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: product, // change
+            key: 'productID'
+        },
+        onDelete: 'CASCADE'
+    },
+    billID: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: product, // change
+            key: 'productID'
+        },
+        onDelete: 'CASCADE'
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    type: {
+        type: DataTypes.ENUM('add', 'subtract', 'modify'),
+        defaultValue: 'subtract',
+    },
+    reason: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+    }
+}, {
+    tableName: 'InventoryTransaction',
+    timestamps: true,
+    paranoid: true,
+});
+
+// Establish associations
+Inventory.hasMany(InventoryDetails, { foreignKey: 'inventoryID' });
+product.hasMany(InventoryDetails, { foreignKey: 'productID' });
+
+InventoryDetails.belongsTo(Inventory, { foreignKey: 'inventoryID' });
+InventoryDetails.belongsTo(product, { foreignKey: 'productID' });
+
+export default InventoryTransaction;
