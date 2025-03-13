092e815b28f0e4a96c44980d6d7a7db0a27403bf
Mann <mannp3005@gmail.com>
2025-03-14 03:52:46 +0530

    Added changes

--- src/utils/sql.js
+++ src/utils/sql.js
@@ -1,9 +1,6 @@
 import { Sequelize } from "sequelize";
+import mysql from 'mysql2';
 const initialSequelize = async() => {
-   
-    console.log(process.env.DATABASE)
-    console.log(process.env.USER)
-    console.log(process.env.PASSWORD)
     const sequelize = new Sequelize(
         process.env.DATABASE,
         process.env.USER,
