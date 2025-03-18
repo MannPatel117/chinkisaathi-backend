56d5a4cf731afa67e9979337759bcb9345d0d90e
Mann <mannp3005@gmail.com>
2025-03-19 02:45:57 +0530

    trial

--- api/app.js
+++ api/app.js
@@ -15,7 +15,9 @@ import inventoryDetails from "./inventoryDetails.js";
 import inventoryAccounts from "./inventoryAccount.js";
 import offers from "./offer.js";
 import bills from "./bill.js";
+import dotenv from "dotenv";
 
+dotenv.config({ path: './.env' });
 // Initialize Express app
 const app = express();
 
@@ -58,5 +60,25 @@ app.use('/api/v1/bills', bills);
 app.get("/", (req, res) => {
   res.status(200).send("Hello From Chinki");
 });
+
+
+import { connectDB } from "../db/connection.js";
+import colors from "colors";
+import "../model/association.model.js";
+
+
+// Connect to DB only once
+connectDB()
+  .then(() => {
+    console.log("Database connection successful!".bgGreen.white);
+
+    // Optional: Handle app-level errors
+    app.on("error", (error) => {
+      console.log(`Server Error: ${error}`.bgRed.white);
+    });
+  })
+  .catch((err) => {
+    console.log(`Connection Failed: ${err}`.bgRed.white);
+  });
 
 export default serverless(app);
