44cf064d1059c66655879e58018e8d2ce781a6a4
Mann <mannp3005@gmail.com>
2025-03-19 02:25:21 +0530

    updated

--- utils/sql.js
+++ utils/sql.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.HOST,
     dialect: "mysql",
+    dialectModule: require('mysql2'),
     dialectOptions: {
       connectTimeout: 60000, // 60 seconds timeout
     },
